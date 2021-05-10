3926e632fa555da8b38049f93cc200bab4a864d2
parvathinandan <61867180+parvathinandan@users.noreply.github.com>
2021-05-10 16:41:50 +0530

    This is my frist commit

---
+++ src/main/java/com/info/jwt/api/GitMergedemoApplication.java
@@ -0,0 +1,13 @@
+package com.info.jwt.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMergedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitMergedemoApplication.class, args);
+	}
+
+}
