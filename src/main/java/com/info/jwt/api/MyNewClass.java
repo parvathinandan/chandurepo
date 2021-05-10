54223bca8e72f377645b2ae9b69365dbd0ea6a35
parvathinandan <61867180+parvathinandan@users.noreply.github.com>
2021-05-10 16:51:18 +0530

    I COLED FRIST TIME

---
+++ src/main/java/com/info/jwt/api/MyNewClass.java
@@ -0,0 +1,12 @@
+
+package com.info.jwt.api;
+
+public class MyNewClass {
+	public void m1() {
+		System.out.println("This is m1() ");
+	}
+
+	public void m2() {
+		System.out.println("I AM DOING THE CLONEING IN THIS TECHNIQUE");
+	}
+}
