224cbdea0f91f770574844a9e988005fc74553b3
parvathinandan <61867180+parvathinandan@users.noreply.github.com>
2021-05-10 17:15:07 +0530

    helloooooooooooooooooo

--- src/main/java/com/info/jwt/api/GitMergedemoApplication.java
+++ src/main/java/com/info/jwt/api/GitMergedemoApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitMergedemoApplication {
 
+	private Integer mnb = 23 ;
 	public static void main(String[] args) {
+		Integer jh = 9;
+		System.out.println("i am jkc");
 		SpringApplication.run(GitMergedemoApplication.class, args);
 	}
 
